17eaac1876a57fc4e721e536c2d201998fd785ad
Emiliano Gomez <emi.gblotta@gmail.com>
2020-10-22 11:00:21 -0300

    mainpage commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { TasksComponent } from "./components/tasks/tasks.component";
+import { PrivateTasksComponent } from "./components/private-tasks/private-tasks.component";
+import { SigninComponent } from "./components/signin/signin.component";
+import { SignupComponent } from "./components/signup/signup.component";
+
+//rutas
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'    
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'private',
+    component: PrivateTasksComponent
+  },
+  {
+    path:'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  }
+];     
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
